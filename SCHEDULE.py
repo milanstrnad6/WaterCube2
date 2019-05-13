2dcf6b6c5d43fc0580b933a72b2c578173a01a9e
milanstrnad6 <milanstrnad6@gmail.com>
2019-05-13 21:33:29 +0200

    Update from dataplicity

--- SCHEDULE.py
+++ SCHEDULE.py
@@ -15,36 +15,35 @@ def pourIfNeeded():
 
     isScheduleEnabled = DATA.load_enabled()
     if isScheduleEnabled == 1:
+	next = TIMES.dateFrom(DATA.load_date())
+	print("SCHEDULE - POUR IF NEEDED - NEXT:")
+	print(next)
 
-		next = TIMES.dateFrom(DATA.load_date())
-		print("SCHEDULE - POUR IF NEEDED - NEXT:")
-		print(next)
+	now = datetime.datetime.now()
+	print("SCHEDULE - POUR IF NEEDED - NOW:")
+	print(now)
 
-		now = datetime.datetime.now()
-		print("SCHEDULE - POUR IF NEEDED - NOW:")
-		print(now)
+	skipDays = DATA.load_skipDays()
 
-		skipDays = DATA.load_skipDays()
+	if now.date() == next.date() and now.time().hour == next.time().hour and now.time().minute == next.time().minute:
+	    print("SCHEDULE - POUR IF NEEDED - [IT IS TIME!]")
 
-		if now.date() == next.date() and now.time().hour == next.time().hour and now.time().minute == next.time().minute:
-	    	print("SCHEDULE - POUR IF NEEDED - [IT IS TIME!]")
+	    #Check water!
 
-	    	#Check water!
+	    #Pouring...
+	    HISTORY.save_automaticPour(duration)
+	    PUMP.start(DATA.load_duration())
 
-	    	#Pouring...
-	    	HISTORY.save_automaticPour(duration)
-	    	PUMP.start(DATA.load_duration())
+	    #Prepare next.
+	    scheduled = next + datetime.timedelta(days=skipDays+1)
+	    DATA.save_date(TIMES.stringFrom(scheduled))
 
-	    	#Prepare next.
-	    	scheduled = next + datetime.timedelta(days=skipDays+1)
-	    	DATA.save_date(TIMES.stringFrom(scheduled))
+	elif now > next:
+	    print("SCHEDULE - POUR IF NEEDED - [NEED TO UPDATE AUTOMATIC_POUR_SCHEDULED DATE]")
 
-		elif now > next:
-	  		print("SCHEDULE - POUR IF NEEDED - [NEED TO UPDATE AUTOMATIC_POUR_SCHEDULED DATE]")
-	  		
-	  		while next <= now:
-			next = next + datetime.timedelta(days=skipDays+1)
-			DATA.save_date(TIMES.stringFrom(next))
+	    while next <= now:
+	    	next = next + datetime.timedelta(days=skipDays+1)
+	    DATA.save_date(TIMES.stringFrom(next))
 
-		else:
-	    	print("SCHEDULE - POUR IF NEEDED - [IT IS NOT TIME YET]")
+	else:
+	    print("SCHEDULE - POUR IF NEEDED - [IT IS NOT TIME YET]")
