425a6a1eb976163212f5d9d039db350af090203e
Milan Strnad <milan.strnad@firma.seznam.cz>
2019-05-17 16:55:34 +0200

    Please

--- MEASUREMENTS.py
+++ MEASUREMENTS.py
@@ -8,8 +8,8 @@ import ULTRASOUND2
 
 #PROPERTIES
 
-NUMBER_OF_MEASUREMENTS = 10
-DIFF_TOLERANCE = 0.3 #CM
+NUMBER_OF_MEASUREMENTS = 20
+# DIFF_TOLERANCE = 0.3 #CM
 
 #ACTIONS
 
@@ -31,17 +31,20 @@ def measure():
     average = totalsum / float(count)
     print("MEASUREMENTS - MEASURE - AVERAGE = [%.3f]" % average)
 
-    minimum = min(measurements)
-    maximum = max(measurements)
-    minOffset = average - minimum
-    print("MEASUREMENTS - MEASURE - MIN OFFSET = [%.3f]" % minOffset)
-    maxOffset = maximum - average
-    print("MEASUREMENTS - MEASURE - MAX OFFSET = [%.3f]" % maxOffset)
+    # minimum = min(measurements)
+    # maximum = max(measurements)
+    # minOffset = average - minimum
+    # print("MEASUREMENTS - MEASURE - MIN OFFSET = [%.3f]" % minOffset)
+    # maxOffset = maximum - average
+    # print("MEASUREMENTS - MEASURE - MAX OFFSET = [%.3f]" % maxOffset)
 
-    if minOffset > DIFF_TOLERANCE:
-        measure()
-    elif maxOffset > DIFF_TOLERANCE:
-        measure()
-    else:
-        DATA.save_distance(average)
-        VOLUME.update()    
+    # if minOffset > DIFF_TOLERANCE:
+    #     measure()
+    # elif maxOffset > DIFF_TOLERANCE:
+    #     measure()
+    # else:
+    #     DATA.save_distance(average)
+    #     VOLUME.update()    
+
+    DATA.save_distance(average)
+    VOLUME.update()
