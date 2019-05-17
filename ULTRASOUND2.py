0a79bbc977a7f843659ff0a5e2f98aa8f3bf7766
Milan Strnad <milan.strnad@firma.seznam.cz>
2019-05-17 16:19:25 +0200

    Try new x

--- ULTRASOUND2.py
+++ ULTRASOUND2.py
@@ -13,14 +13,32 @@ ECHO=24
 high_tick = None # global to hold high tick.
 
 def cbfunc(gpio, level, tick):
-   global high_tick
-   if level == 0: # echo line changed from high to low.
-      if high_tick is not None:
-         echo = pigpio.tickDiff(high_tick, tick)
-         cms = (echo / 1000000.0) * 34030 / 2
-         print("echo was {} micros long ({:.1f} cms)".format(echo, cms))
-   else:
-      high_tick = tick
+	global high_tick
+	if level == 0: # echo line changed from high to low.
+		if high_tick is not None:
+			echo = pigpio.tickDiff(high_tick, tick)
+			cms = (echo / 1000000.0) * 34030 / 2
+			print("echo was {} micros long ({:.1f} cms)".format(echo, cms))
+	else:
+		high_tick = tick
+
+# def getDistance():
+# 	pi = pigpio.pi() # Connect to local Pi.
+
+# 	pi.set_mode(TRIGGER, pigpio.OUTPUT)
+# 	pi.set_mode(ECHO, pigpio.INPUT)
+
+# 	cb = pi.callback(ECHO, pigpio.EITHER_EDGE, cbfunc)
+
+# 	start = time.time()
+
+# 	while (time.time()-start) < 60:
+#    		pi.gpio_trigger(TRIGGER, 10)
+#    		time.sleep(0.2)
+
+# 	cb.cancel() # Cancel callback.
+# 	pi.stop() # Close connection to Pi
+
 
 pi = pigpio.pi() # Connect to local Pi.
 
@@ -29,11 +47,8 @@ pi.set_mode(ECHO, pigpio.INPUT)
 
 cb = pi.callback(ECHO, pigpio.EITHER_EDGE, cbfunc)
 
-start = time.time()
-
-while (time.time()-start) < 60:
-   pi.gpio_trigger(TRIGGER, 10)
-   time.sleep(0.2)
+time.sleep(0.2)
+pi.gpio_trigger(TRIGGER, 10)
 
 cb.cancel() # Cancel callback.
 pi.stop() # Close connection to Pi
