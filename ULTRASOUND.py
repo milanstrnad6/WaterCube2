6559c1a57aa344685b8388f6abfdf6a2fb3b1414
Milan Strnad <milan.strnad@firma.seznam.cz>
2019-05-17 16:02:09 +0200

    Try 5

--- ULTRASOUND.py
+++ ULTRASOUND.py
@@ -27,7 +27,7 @@ def getDistance():
 
     #Sending sound for duration 0.00001 seconds.
     IO.output(US_OUT, 1)
-    time.sleep(0.001) #0.00001
+    time.sleep(0.00005) #0.00001
     IO.output(US_OUT, 0)
 
     #Prepare properties for measuring time.
@@ -47,7 +47,8 @@ def getDistance():
     #Calculate distance.
     timeElapsed = timeStop - timeStart
     print("timeElapsed = %.10f" % timeElapsed)
-    distance = (timeElapsed * 34300) / 2 # 34300
+    #distance = (timeElapsed * 34300) / 2 # 34300
+    distance = timeElapsed / 29.1
 
     print("ULTRASOUND - GET DISTANCE... [%.3f CM]" % distance)
     return distance
