a1017e03df56faaf20e674ba7674bed305c409fe
Milan Strnad <milan.strnad@firma.seznam.cz>
2019-05-20 11:20:43 +0200

    Update

--- APNS.py
+++ APNS.py
@@ -10,30 +10,16 @@ CERTIFICATE = '</home/pi/CUBE/DATA/certificate.pem>'
 
 #MAIN
 
-client = APNSClient(certificate=CERTIFICATE,
-                    default_error_timeout=10,
-                    default_expiration_offset=2592000,
-                    default_batch_size=100,
-                    default_retries=5)
+sendNotification():
+	client = APNSClient(certificate=CERTIFICATE,default_error_timeout=10,default_expiration_offset=2592000,default_batch_size=100,default_retries=5)
 
-token = DATA.load_token()
-alert = 'Water level below 20%, please refill.'
+	token = DATA.load_token()
+	alert = 'Water level below 20%, please refill.'
 
-# Send to single device.
-# NOTE: Keyword arguments are optional.
-res = client.send(token,
-                  alert,
-                  badge='badge count',
-                  sound='sound to play',
-                  category='category',
-                  content_available=True,
-                  title='WARNING',
-                  title_loc_key='t_loc_key',
-                  title_loc_args='t_loc_args',
-                  action_loc_key='a_loc_key',
-                  loc_key='loc_key',
-                  launch_image='path/to/image.jpg',
-                  extra={'custom': 'data'})
+	# Send to single device.
+	# NOTE: Keyword arguments are optional.
+	res = client.send(token,alert,badge='badge count',sound='sound to play',category='category',content_available=True,title='WARNING',title_loc_key='t_loc_key',title_loc_args='t_loc_args',action_loc_key='a_loc_key',loc_key='loc_key',launch_image='path/to/image.jpg',extra={'custom': 'data'})
 
-# Send to multiple devices by passing a list of tokens.
-#client.send([token], alert, **options)
+	# Send to multiple devices by passing a list of tokens.
+	#client.send([token], alert, **options)
+
