de141b7ca593dced61fe590f22d108b264b6b0cb
Milan Strnad <milan.strnad@firma.seznam.cz>
2019-06-17 11:00:55 +0200

    Change text

--- APNS.py
+++ APNS.py
@@ -18,7 +18,7 @@ def sendNotification():
 	client = APNSSandboxClient(certificate=CERTIFICATE,default_error_timeout=10,default_expiration_offset=2592000,default_batch_size=100,default_retries=5)
 
 	token = DATA.load_token()
-	alert = 'Water level below 20%, please refill.'
+	alert = 'Bonsai: Water level below 20%, please refill.'
 
 	# Send to single device.
 	# NOTE: Keyword arguments are optional.
@@ -29,7 +29,7 @@ def sendNotification():
 		sound='sound to play',
 		category='category',
 		content_available=True,
-		title='WARNING - CUBE: Bonsai',
+		title='WARNING',
 		title_loc_key='t_loc_key',
 		title_loc_args='t_loc_args',
 		action_loc_key='a_loc_key',
